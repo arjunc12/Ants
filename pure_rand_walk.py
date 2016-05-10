90c91328289d792a6d4467ef29505a302076f568
Arjun <arjunc12@gmail.com>
2016-05-10 15:13:21 -0700

    changed pure random walk to act on full graph

--- pure_rand_walk.py
+++ pure_rand_walk.py
@@ -77,6 +77,23 @@ def fig1_network():
 
     return G
 
+def full_grid():
+    G = nx.grid_2d_graph(11,11)
+    G.remove_edge((4, 5), (5, 5))
+
+    for i,u in enumerate(G.nodes_iter()):
+        M[i] = u
+        Minv[u] = i
+
+    for i, (u,v) in enumerate(G.edges()):
+        Ninv[(u, v)] = i
+        N[i] = (u, v)        
+        Ninv[(v, u)] = i
+            
+    for (u, v) in G.edges():
+        assert (u, v) in Ninv
+    return G
+
 def walk_to_string(walk):
     walk_str = []
     for i in range(len(walk)):
@@ -84,8 +101,8 @@ def walk_to_string(walk):
     return '-'.join(walk_str)
 
 def pure_random_walk(G, nest, target, ants):
-    out_file = open('pure_random_walk.csv', 'a')
-    for ant in range(ants):
+    out_file = open('pure_rand_walk.csv', 'a')
+    for ant in xrange(ants):
         curr = nest
         steps = 0
         walk = [curr]
@@ -104,8 +121,11 @@ def pure_random_walk(G, nest, target, ants):
     out_file.close()
     
 if __name__ == '__main__':
-    G = fig1_network()
-    target = (3,2)
-    nest = (8,3)
+    #G = fig1_network()
+    G = full_grid()
+    #target = (3,2)
+    #nest = (8,3)
+    target = (0,5)
+    nest = (10,5)
     ants = int(argv[1])
     pure_random_walk(G, nest, target, ants)
