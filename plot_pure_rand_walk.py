79919ef4bf44afe21cc5e619160e3d3237806dbb
Arjun <arjunc12@gmail.com>
2016-05-10 15:11:40 -0700

    added axis labels

--- plot_pure_rand_walk.py
+++ plot_pure_rand_walk.py
@@ -3,7 +3,11 @@ import pylab
 from collections import defaultdict
 from scipy.stats import entropy
 
-df = pd.read_csv('pure_random_walk.csv', header=None, names = ['steps', 'path'])
+df = pd.read_csv('pure_rand_walk.csv', header=None, names = ['steps', 'path'])
+#print max(df['steps'])
+df2 = df[df['steps'] <= 1000]
+print len(df2.index), len(df.index)
+'''
 path_counts = defaultdict(int)
 N = 0
 for path in df['path']:
@@ -12,4 +16,7 @@ for path in df['path']:
 etr = entropy(path_counts.values())
 pylab.hist(df['steps'])
 pylab.title("N = %d, mean = %0.2f, median = %d, entropy=%0.2f" % (N, pylab.mean(df['steps']), pylab.median(df['steps']), etr))
+pylab.xlabel('Steps to reach target from nest')
+pylab.ylabel('Count')
 pylab.savefig('pure_rand_walk.png', format='png')
+'''
