66ac10dbb340a3c24cfd4b7b6042b1876a41295a
Arjun <arjunc12@gmail.com>
2016-11-23 17:11:20 -0800

    code for plotting the average weight over time of the two critical edges in the minimal working example

---
+++ plot_critical_edges.py
@@ -0,0 +1,10 @@
+import pylab
+import pandas as pd
+
+df = pd.read_csv('critical_edges.csv', header=False, names=['step', 'w1', 'w2'])
+
+df = df.groupby('step', as_index=False).agg(pylab.mean)
+
+pylab.plot(df['step'], df['w1'], c='b')
+pylab.plot(df['step'], df['w2'], c='r')
+pylab.savefig('critical_edges.png', format='png')
