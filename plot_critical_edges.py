c5aa90594b2623d332108d13ced1648b4c8818dd
Arjun <arjunc12@gmail.com>
2016-11-28 12:03:17 -0800

    added code to plot sum of the two critical edges

--- plot_critical_edges.py
+++ plot_critical_edges.py
@@ -7,4 +7,5 @@ df = df.groupby('step', as_index=False).agg(pylab.mean)
 
 pylab.plot(df['step'], df['w1'], c='b')
 pylab.plot(df['step'], df['w2'], c='r')
+pylab.plot(df['step'], df['w1'] + df['w2'], c='m')
 pylab.savefig('critical_edges.png', format='png')
