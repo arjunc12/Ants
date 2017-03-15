72beebe481fd5b5866d38a7fa9c980cc0ebf344e
Arjun <arjunc12@gmail.com>
2017-03-15 16:56:39 -0700

    added command line options for looking at simulation results that allowed backtracking and/or unidirectional search

--- get_metrics.py
+++ get_metrics.py
@@ -17,6 +17,8 @@ parser.add_argument('-g', '--graphs', required=True, nargs='+')
 parser.add_argument('-dt', '--decay_types', required=True, nargs='+')
 parser.add_argument('-m', '--max_steps', type=int, required=True, nargs='+')
 parser.add_argument('-v', '--var', action='store_true')
+parser.add_argument('-b', '--backtrack', action='store_true')
+parser.add_argument('-o', '--one_way', action='store_true')
 
 args = parser.parse_args()
 explores = args.explores
@@ -27,12 +29,20 @@ graphs = args.graphs
 decay_types = args.decay_types
 max_steps = args.max_steps
 var = args.var
+backtrack = args.backtrack
+one_way = args.one_way
 
 for strategy in strategies:
     for graph in graphs:
         for decay_type in decay_types:
             for steps in max_steps:
-                fname = 'ant_repair_%s_%s_%s%d.csv' % (strategy, graph, decay_type, steps)
+                out_items = ['repair', strategy, graph_name, decay_type]
+                if backtrack:
+                    out_items.append('backtrack')
+                if one_way:
+                    out_items.append('one_way')
+                out_str = '_'.join(out_items)
+                fname = 'ant_%s%d.csv' % (out_str, max_steps)
                 print fname
                 df = pd.read_csv(fname, header=None, names = columns, skipinitialspace=True)
                 df = df[['explore', 'decay'] + metrics]
