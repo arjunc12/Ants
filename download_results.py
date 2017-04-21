2500a6fa549a75d11c3cfa659ace79d9876b7c13
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2017-04-21 10:47:41 -0700

    makes directories, puts downloaded files into directories in organized manner

--- download_results.py
+++ download_results.py
@@ -7,8 +7,9 @@ parser.add_argument('-g', '--graphs', nargs='+', required=True)
 parser.add_argument('-dt', '--decay_types', nargs='+', required=True)
 parser.add_argument('-me', '--metrics', nargs='+', required=True)
 parser.add_argument('-l', '--labels', nargs='+', required=True)
+parser.add_argument('--sandbox', action='store_true')
 
-remote_dir = 'achandrasekhar@doritos.snl.salk.edu:/home/achandrasekhar/Documents/'
+remote_dir = 'achandrasekhar@doritos.snl.salk.edu:/home/achandrasekhar/Documents'
 
 args = parser.parse_args()
 strategies = args.strategies
@@ -16,17 +17,27 @@ graphs = args.graphs
 decay_types = args.decay_types
 metrics = args.metrics
 labels = args.labels
+sandbox = args.sandbox
 
 for metric in metrics:
     for strategy in strategies:
         for graph in graphs:
             for decay_type in decay_types:
                 for label in labels:
-                    local_dir = 'figs/%s/%s/%s/' % (metric, strategy, label)
+                    local_dir = ['figs']
+                    if sandbox:
+                        local_dir.append('sandbox')
+                    local_dir += [metric, strategy, label]
+                    local_dir = '/'.join(local_dir)
                     mkdir_command = 'mkdir -p %s' % local_dir
                     print mkdir_command
                     os.system(mkdir_command)
-                    fname = '%s_repair_%s_%s_%s%s.pdf' % (metric, strategy, graph, decay_type, label)
+                    prefix_items = [metric, 'repair']
+                    if sandbox:
+                        prefix_items.append('sandbox')
+                    prefix_items += [strategy, graph, decay_type]
+                    prefix = '_'.join(prefix_items)
+                    fname = '%s%s.pdf' % (prefix, label)
                     scp_command = 'scp %s/%s %s/%s' % (remote_dir, fname, local_dir, fname)
                     print scp_command
                     os.system(scp_command)                    
