1f8d5f9c6910053b29b31e3a7c786f200cb08eb1
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-06-07 14:09:38 -0700

    code for plotting with finite/infinite ants

--- plot_test_repellant.py
+++ plot_test_repellant.py
@@ -3,7 +3,7 @@ mpl.use('agg')
 import pandas as pd
 import pylab
 
-def plot_repellant(repellant_df):
+def plot_repellant(repellant_df, label):
     df1 = repellant_df[repellant_df['lay_back'] == 1]
     df2 = repellant_df[((repellant_df['lay_back'] == 0) & (repellant_df['pluck_rate'] > 0))]
     df3 = repellant_df[((repellant_df['lay_back'] == 0) & (repellant_df['pluck_rate'] == 0))]
@@ -17,11 +17,15 @@ def plot_repellant(repellant_df):
         df = df.groupby('pluck_rate', as_index=False).agg(pylab.mean)
         color = colors[i]
         pylab.scatter(df['pluck_rate'], df['steps'], c=color)
-    pylab.savefig('test_repellant.pdf', format='pdf')
+    pylab.savefig('test_repellant%s.pdf' % label, format='pdf')
 
 def main():
-    repellant_df = pd.read_csv('test_repellant.csv', names=['steps', 'pluck_rate', 'lay_back'])
-    plot_repellant(repellant_df) 
+    #repellant_df1 = pd.read_csv('test_repellant.csv', names=['steps', 'pluck_rate', 'lay_back'])
+    #plot_repellant(repellant_df1, '1') 
+    
+    repellant_df2 = pd.read_csv('test_repellant2.csv', names=['steps', 'pluck_rate', 'lay_back'])
+    #print repellant_df2
+    plot_repellant(repellant_df2, '2') 
 
 if __name__ == '__main__':
     main()
