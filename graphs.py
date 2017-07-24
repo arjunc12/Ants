f622bd9d028d79259b6a265a2c0318a7e41b9658
arjun chandrasekhar <arjunchandrasekhar@arjuns-MacBook-Pro.local>
2017-07-24 15:49:49 -0700

    random difficulties on same spanning trees

--- graphs.py
+++ graphs.py
@@ -130,21 +130,18 @@ def spanning_trees():
     H.graph['nests'] = G.graph['nests']
     H.graph['name'] = 'span_trees'
     
-    #H = partition_plants(H)
-    #H.graph['name'] = 'span_trees'
+    '''
+    H = partition_plants(H)
+    H.graph['name'] = 'span_trees'
+    '''
     
-    '''
-    for u, v in H.edges_iter():
-        if H.node[u]['plant'] != H.node[v]['plant']:
-            H[u][v]['difficulty'] = 2
-    '''
-    #assign_difficulties(H)
+    assign_difficulties(H)
     
     for i in xrange(11):
         u, v = (4, i), (5, i)
         H.add_edge(u, v)
         H[u][v]['difficulty'] = 3
-        
+
     '''
     for i in xrange(10):
         u, v = (i, 5), (i + 1, 5)
