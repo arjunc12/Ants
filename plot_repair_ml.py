5a0f508c96172f56db34cfb2ea3753375cc164d0
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-08-09 13:07:50 -0700

    added function for finding the ml parameters without plotting

--- plot_repair_ml.py
+++ plot_repair_ml.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import numpy as np
 import pylab
 
-COLUMNS = ['sheet', 'strategy', 'decay_type', 'ghost', 'explore', 'decay',\
+COLUMNS = ['sheet', 'num_lines', 'strategy', 'decay_type', 'ghost', 'explore', 'decay',\
            'likelihood']
 
 NOCUT = ['6', '7a', '7c', '8a', '8b', '9b', '14','15', '16', '17', '21a', '21b', '23a', '23d']
@@ -19,6 +19,25 @@ ALL2 = NOCUT2 + CUT
 DATASETS_DIR = 'datasets/reformated_csv'
 OUT_DIR = 'ml_plots'
 ML_OUTFILE = '%s/repair_ml.csv' % OUT_DIR
+
+def print_ml_parameters(df, strategies, decay_types):
+    df2 = df[(df['strategy'].isin(strategies)) & (df['decay_type'].isin(decay_types))]
+    for name, group in df2.groupby(['strategy', 'decay_type']):
+        print "%s, %s" % (strategy, decay_type)
+        strategy, decay_type = name
+        max_likelihood = float('-inf')
+        max_likelihood_params = []
+        for name2, group2 in df2.groupby(['explore', 'decay']):
+            explore, decay = name2
+            likelihood = sum(group2['likelihood'])
+            if likelihood > max_likelihood:
+                max_likelihood = likelihood
+                max_likelihood_params = [(explore, decay)]
+            elif likelihood == max_likelihood:
+                max_likelihood_params.append((explore, decay))
+        print "maximum likelihood %f" % max_likelihood
+        for explore, decay in max_likelihood_params:
+            print "e = %f, d=%f" % (explore, decay)
 
 def ml_heat(df, strategies, decay_types):
     df2 = df[(df['strategy'].isin(strategies)) & (df['decay_type'].isin(decay_types))]
@@ -51,9 +70,20 @@ def ml_plot(df, strategies, decay_types):
         pylab.close()
 
 def filter_df(df, emin, emax, dmin, dmax, ghost, sheets):
+    '''
     df =  df[(emin <= df['explore']) & (df['explore'] <= emax) & \
              (dmin <= df['decay']) & (df['decay'] <= dmax) & \
              (df['ghost'] == int(ghost)) & (df['sheet'].isin(sheets))]
+    '''
+    df = df[df['explore'] >= emin]
+    df = df[df['explore'] <= emax]
+    
+    df = df[df['decay'] >= dmin]
+    df = df[df['decay'] <= dmax]
+    
+    df = df[df['ghost'] == int(ghost)]
+   
+    df = df[df['sheet'].isin(sheets)]
 
     return df
 
@@ -75,6 +105,7 @@ def main():
     parser.add_argument('--heat', action='store_true')
     parser.add_argument('--plot', action='store_true')
     parser.add_argument('--hist', action='store_true')
+    parser.add_argument('--print', action='store_true', dest='print_ml')
 
     args = parser.parse_args()
 
@@ -95,6 +126,11 @@ def main():
         sheets = ALL
     elif sheets == ['all2']:
         sheets = ALL2
+    else:
+        assert len(sheets) > 1
+        acceptable_sheets = set(ALL)
+        for sheet in sheets:
+            assert sheet in acceptable_sheets
    
     strategies = args.strategies
     decay_types = args.decay_types
@@ -110,14 +146,18 @@ def main():
     heat = args.heat
     plot = args.plot
     hist = args.hist
+    print_ml = args.print_ml
 
     df = pd.read_csv(ML_OUTFILE, names = COLUMNS, skipinitialspace=True)
+    df['sheet'] = df['sheet'].astype(str)
     df = filter_df(df, emin, emax, dmin, dmax, ghost, sheets)
 
     if heat:
         ml_heat(df, strategies, decay_types)
     if plot:
         ml_plot(df, strategies, decay_types)
+    if print_ml:
+        print_ml_parameters(df, strategies, decay_types)
 
 if __name__ == '__main__':
     main()
