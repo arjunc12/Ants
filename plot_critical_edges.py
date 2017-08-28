ca01d9fb1e2376b126b3963873fa9d39107a49c1
Arjun Chandrasekhar <achandrasekhar@snl.salk.edu>
2017-08-28 11:58:26 -0700

    plot weights function does all the plotting instead of just one plot

--- plot_critical_edges.py
+++ plot_critical_edges.py
@@ -1,29 +1,38 @@
+import matplotlib as mpl
+mpl.use('agg')
 import pylab
 import pandas as pd
 from sys import argv
+import argparse
 
-def plot_edge_weights(df, limit):
-    df = df.groupby('step', as_index=False).agg(pylab.mean)
-    
-    pylab.figure()
-    pylab.plot(df['step'], df['w1'], c='b', label='dead end')
-    pylab.plot(df['step'], df['w2'], c='r', label='path')
-    pylab.plot(df['step'], df['w1'] + df['w2'], c='m', label='total')
-    pylab.legend()
-    title_str = 'critical_edges%d' % limit
-    pylab.savefig('%s.png' % title_str, format='png')
-    pylab.close()
+def plot_edge_weights(graph_name, limits):
+    df = pd.read_csv('critical_edges_%s.csv' % graph_name, header=None, names=['step', 'w1', 'w2'])
 
-df = pd.read_csv('critical_edges.csv', header=False, names=['step', 'w1', 'w2'])
+    limits = reversed(sorted(limits))
+    for limit in limits:
+        df2 = df[df['step'] <= limit]
+        df2 = df.groupby('step', as_index=False).agg(pylab.mean)
+        
+        pylab.figure()
+        pylab.plot(df2['step'], df2['w1'], c='b', label='dead end')
+        pylab.plot(df2['step'], df2['w2'], c='r', label='path')
+        #pylab.plot(df2['step'], df2['w1'] + df2['w2'], c='m', label='total')
+        pylab.legend()
+        title_str = 'critical_edges_%s%d' % (graph_name, limit)
+        pylab.savefig('%s.pdf' % title_str, format='pdf')
+        pylab.close()
 
-max_steps = []
-if len(argv) > 1:
-    max_steps = map(int, argv[1:])
-else:
-    max_steps.append(max(df['step']))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--graphs', nargs='+')
+    parser.add_argument('-l', '--limits', nargs='+', type=int)
 
-max_steps = reversed(sorted(max_steps))
+    args = parser.parse_args()
+    graphs = args.graphs
+    limits = args.limits
 
-for limit in max_steps:
-    df = df[df['step'] <= limit]
-    plot_edge_weights(df, limit)
+    for graph in graphs:
+        plot_edge_weights(graph, limits)
+
+if __name__ == '__main__':
+    main()
